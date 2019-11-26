5303fb87228360168868d0f02053c1126a4dabcc
castanc <54039892+castanc@users.noreply.github.com>
2019-11-26 17:04:38 -0300

    course part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module'
+import { FormsModule } from '@angular/forms';
+import { SidenavExampleComponent } from './sidenav-example/sidenav-example.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavExampleComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
