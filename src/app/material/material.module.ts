ebea739ae5ababc3dce5436629e7bf191c75901f
castanc <54039892+castanc@users.noreply.github.com>
2019-11-26 17:02:09 -0300

    course part 1

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,43 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material'
+import { MatButtonToggleModule } from '@angular/material'
+import { MatIconModule } from '@angular/material'
+import { MatProgressSpinnerModule } from '@angular/material'
+import { MatToolbarModule } from '@angular/material'
+import { MatSidenavModule } from '@angular/material'
+import { MatMenuModule } from '@angular/material'
+import { MatListModule } from '@angular/material'
+import { MatDividerModule } from '@angular/material'
+import { MatGridListModule } from '@angular/material'
+import { MatExpansionModule } from '@angular/material'
+import { MatCardModule } from '@angular/material'
 
-const MaterialComponents =[
+
+
+
+import { MatBadgeModule } from '@angular/material/badge'
+
+
+
+const material =[
   MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatMenuModule,
+  MatListModule,
+  MatDividerModule,
+  MatGridListModule,
+  MatExpansionModule,
+  MatCardModule,
 ]
 
 @NgModule({
   declarations: [],
-  imports: [MaterialComponents],
-  exports: [MaterialComponents]
+  imports: [material],
+  exports: [material]
 })
 export class MaterialModule { }
