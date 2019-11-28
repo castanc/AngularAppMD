0ad7a001cf98d0f54849f39bac8e73bccde5a4e1
castanc <54039892+castanc@users.noreply.github.com>
2019-11-28 15:46:53 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent, CustomSnackBarComponent } from './app.component';
+import { AppComponent  } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module'
 import { FormsModule , ReactiveFormsModule} from '@angular/forms';
+import { CustomSnackbarComponent } from './custom-snackbar/custom-snackbar.component';
+import { DialogExampleComponent } from './dialog-example/dialog-example.component';
+import { ScrollingModule } from '@angular/cdk/scrolling'
+import { APP_BASE_HREF, LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    CustomSnackbarComponent,
+    DialogExampleComponent,
   ],
+  entryComponents: [CustomSnackbarComponent,DialogExampleComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -19,10 +26,12 @@ import { FormsModule , ReactiveFormsModule} from '@angular/forms';
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    CustomSnackBarComponent,
+    ScrollingModule
+    
   ],
-  entryComponents: [CustomSnackBarComponent],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
